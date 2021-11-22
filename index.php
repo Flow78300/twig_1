0671a2ca15ba30b36a8e7822ed04247eda405bc7
Florian Dugué <79923290+Flow78300@users.noreply.github.com>
2021-11-22 12:20:36 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Get a $twig object from this file.
-require_once __DIR__ . '/../config/twig.php';
-require __DIR__ . '/../public/products.php';
-
-$name = 'Wilder';
-
-echo $twig->render('home.html.twig', ['name' => $name]);
